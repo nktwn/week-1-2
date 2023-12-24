9bf1d5f7915fdbe20034c65acc35bb8664593d46
Nurkhat <113333558+nktwn@users.noreply.github.com>
2023-12-24 22:04:40 +0600

    2

--- main.go
+++ main.go
@@ -1,3 +1,53 @@
-package advancedProg
+package main
 
-//1
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type JsonRequest struct {
+	Message string `json:"message"`
+}
+
+type JsonResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/nurkhat", handlePostRequest)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestData JsonRequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	if requestData.Message == "" {
+		http.Error(w, "Invalid JSON message", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("Received message:", requestData.Message)
+
+	response := JsonResponse{
+		Status:  "success",
+		Message: "Data successfully received",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(response)
+}
